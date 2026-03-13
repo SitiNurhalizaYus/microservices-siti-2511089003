27f20a251d723f0fc6ade512473d43be2ea81192
Siti Nurhaliza Yus <sitinurhalizayus21@gmail.com>
2026-03-13 22:41:00 +0700

    feat: buat model dan repository product service

---
+++ product/src/main/java/com/siti/product/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.siti.product.repository;
+
+import com.siti.product.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
